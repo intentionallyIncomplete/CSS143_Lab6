6aa8a23ef4e840235856803d2ff9f0d6caa9d3f9
Ian Bryan <ianbryanz@gmail.com>
2018-11-05 02:39:34 -0800

    Sending to Laptop

--- src/RecursionLab.java
+++ src/RecursionLab.java
@@ -22,96 +22,109 @@
 import java.awt.Dimension;
 
 import javax.swing.*;
-	
+
+/**
+ * Class Description:
+ * @author Ian Bryan
+ * @version 11/04/2018
+ * 
+ * <p>
+ * 
+ * </p>
+ */
 public class RecursionLab {
-	
+	/*Class level data members and statics*/
+	/*
+	 * JTextArea creates a new canvas
+	 * static count 
+	 * */
 	private static JTextArea myArea = new JTextArea();
 	private static int count = 0;
-	
-	public static void main( String args[] ) {	//invoke the recursive method here...
-		
-		/**
-		 * TODO: switch between the two commented lines below and execute this code, 
-		 * observing the output for both the iterative solution and the recursive solution.
-		 * To watch the recursive behaviour in action, set a breakpoint on the if statement
-		 * inside the recursiveSum() function
-		 * 
-		 */
-		int solution = iterativeSum( 20 );
-		//int solution = recursiveSum( 20 );
-		
-		
+
+
+	@SuppressWarnings("javadoc")
+	public static void main(String args[]) {
+		// Calls to both functions. Both produce the same result but one is recursive
+		// while the other is iterative.
+		//int solution = iterativeSum( 20 );
+		int solution = recursiveSum( 20 );
+
 		//Some GUI details
 		myArea.setText(("Result is : " + solution + "\n" + myArea.getText()));
 		JScrollPane myPane = new JScrollPane( myArea );
 		myPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		myPane.setPreferredSize(new Dimension(600,300));
 		JOptionPane.showMessageDialog( null, myPane );
-		
-		//good form to include an exit call when GUIing in Java 
 		System.exit(0);
-	}
-	
-	
-	
-	/** recursion is similar to iterative looping, but we
-	 *  use method calls to repeat computations (or decompose the problem) 
-	 *  instead of explicit looping control structures 
-	 */
-	public static int recursiveSum( int n ) {
-		updateRecursiveDisplay(n);			//overhead for nice output, not required
-		if( n == 1)			//if we're at the base case...
-			return 1;		//then return the answer to the simplest problem which we know how to solve
-		else				//otherwise, we rely on the fact that sum( n ) = n + sum( n - 1 ) and keep recursing
-			return ( n + recursiveSum( n - 1) );
-	}						//for this method to terminate, we must be breaking the problem down into smaller
-							//and smaller problems, until we reach the simplest form of the problem which we know
-							//how to solve (in this case, it's the fact that sum( 1 ) == 1 )
+	}//end main
 
-	//the iterative counterpart to the above recursion
-	//notice how it's longer? At times, an iterative solution may require more code than the recursive counterpart, 
-	//but, the recursive solution is slower and more memory intensive.  We can always recast recursion as iteration.
-	public static int iterativeSum( int i ) {
+	/** 
+	 * @param n
+	 * recursiveSum() function holds the details of what each suspended
+	 * call will use to produce an output to be handed back to the same method. 
+	 */
+	public static int recursiveSum(int n) {
+		//overhead for nice output, not required
+		updateRecursiveDisplay(n);
+		if(n == 1) {
+			return 1;
+		}else {
+			return (n + recursiveSum(n - 1));
+		}		
+	}
+
+	public       00                                                                                                                                                                                                                                                                                                                                                                           
+	/**
+	 * @param i
+	 * @return Returns the same value as the recursive function.
+	 */
+	public static int iterativeSum(int i) {
 		int total = 0;
-		
+
 		for( int n = i; n >= 1; n--) {
 			updateIterativeDisplay(n);
 			total = total + n;
 		}
 		return total;
 	}						
-	
+
+	/**
+	 * @param n
+	 * A given method for printing all the data to the screen in a formatted way.
+	 */
 	public static void updateIterativeDisplay(int n) {
 		count++;
 		String text = myArea.getText();
-		
+
 		text += "\n/*******************Loop iteration " + count + "**************************************";
 		text += "\n Calling iterativeSum( int n = " + n +" ). Total += " + n;
 		text += "\n***************************************************************************/";
-		
+
 		myArea.setText( text );
 	}
-							
-							
-	//ignore this method unless interested in the output string						
+					
+	/**
+	 * @param n
+	 * Building the String of combined data for output processing.
+	 */
 	public static void updateRecursiveDisplay(int n) {
-		
+
 		count++;
 		String text = myArea.getText();
-		
-		
+
+
 		if( count == 1 )  {
 			text += "\n       return ( n + recursiveSum( n - 1 ) ) \n\n";
 			text += "       CALL STACK IN MAIN MEMORY                ";
 		}
-		
-		
+
+
 		text += "\n/*******************Method invocation " + count + "*********************";
-		
-		
+
+
 		text += "\n Calling recursiveSum( int n = " + n +" ). ";
 		text += "\n The return statement from this function will resolve in " + (n-1) + " more recursive method calls...";
-		
+
 		if( n != 1 ) {
 			text += "\n The return statement which invokes the recursive call is \"return ( " + n + " + recursiveSum( "+ (n - 1) +" ));";
 		} else {
@@ -120,13 +133,13 @@ public class RecursionLab {
 			text += "\n information by bubbling up through all of the other, yet-to-be-determined return expressions";
 		}
 		text += "\n***************************************************************************/";
-		
+
 		myArea.setText( text );
-		
+
 	}
 }
-	
 
-	
-	
-	
+
+
+
+
