707034de3c21b8daa51fce1a23e63e07fadaee85
Ian Bryan <ianbryanz@gmail.com>
2018-11-05 14:57:48 -0800

    X^n Recursive Summation
    Added a module that correctly calculates the summation of a base raised to a power based on the exponential sum rule of x^n = x*x^(n-1) when n>0.

--- src/RecursionLab.java
+++ src/RecursionLab.java
@@ -47,8 +47,13 @@ public class RecursionLab {
 		// Calls to both functions. Both produce the same result but one is recursive
 		// while the other is iterative.
 		//int solution = iterativeSum( 20 );
-		int solution = recursiveSum( 20 );
-
+		//int solution = recursiveSum( 20 );
+		
+		/***********************************************/
+		/* Some method calls for modules that use recursion to solve problems*/
+		int solution = expSumRule(2,6);
+		/***********************************************/
+		
 		//Some GUI details
 		myArea.setText(("Result is : " + solution + "\n" + myArea.getText()));
 		JScrollPane myPane = new JScrollPane( myArea );
@@ -58,6 +63,7 @@ public class RecursionLab {
 		System.exit(0);
 	}//end main
 
+	/*			MODULES THAT USE RECURSION			*/
 	/** 
 	 * @param n
 	 * @return recursiveSum() function holds the details of what each suspended
@@ -81,16 +87,14 @@ public class RecursionLab {
 	 * an integer 'base' which is the base to raise from.
 	 * 
 	 * Base Case: When n==0, this is to say "when any number raised to it's 0th power"
-	 * is always 1, so return 1.
+	 * is always the base, so return the base.
 	 */
-	public static int recursiveExponent(int base, int n) {
+	public static int expSumRule(int base, int n) {
 		updateDisaplyForExpRec(base, n);
-		if(n == 0) {
+		if(n == 0 || n == 1) {
 			return base;
-		}else if(n < 0) {
-			return -1;
 		}else {
-			return(base*recursiveExponent(n-1,base));
+			return(base * expSumRule(base, n-1));
 		}
 	}                                                                                                                                                                                                                                                                                                                                                     
 	/**
@@ -107,6 +111,7 @@ public class RecursionLab {
 		return total;
 	}						
 
+	/*		DISPLAY METHODS FOR ABOVE MODULES		*/
 	/**
 	 * @param n
 	 * A given method for printing all the data to the screen in a formatted way.
@@ -133,7 +138,7 @@ public class RecursionLab {
 		String text = myArea.getText();
 
 		if( count == 1 )  {
-			text += "\n       return ( n + recursiveExponent( n - 1 ) ) \n\n";
+			text += "\n       return ( base * expSumRule( n - 1 ) ) \n\n";
 			text += "       CALL STACK IN MAIN MEMORY                ";
 		}
 
@@ -145,18 +150,15 @@ public class RecursionLab {
 		text += "\n The return statement from this function will resolve in " + (n-1) + " more recursive method calls...";
 
 		if( n != 1 ) {
-			text += "\n The return statement which invokes the recursive call is \"return ( " + n + " * exponentialRecursion( "+ (n - 1) +","+base+"));";
+			text += "\n The return statement which invokes the recursive call is \"return ( " + base + " * expSumRule( "+ base +"," + (n-1) + "));";
 		} else {
-			text += "\n The base case has been hit.\nThe return statement is \"return 1;\" which is the value returned to the expression above. ";
-			text += "\n Notice how hitting the base case will provide a solid, known piece of information from which we will construct more known ";
-			text += "\n information by bubbling up through all of the other, yet-to-be-determined return expressions";
+			text += "\n The base case has been hit.\nThe return statement is \"return base;\" which is the value returned to the expression above. ";
 		}
 		text += "\n***************************************************************************/";
 
 		myArea.setText( text );
 	}
 
-	
 	/**
 	 * @param n
 	 * Building the String of combined data for output processing.
